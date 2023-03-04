010a5ca1bd21ac99a25b49fd7d18882740bfff9f
Roger Araujo <araujoroger2402@gmail.com>
2023-03-03 22:18:12 -0400

    se agrega el login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import App from './components/App';
+import login from './components/login';
 
 reactDom.render(<App />, document.getElementById('app'));
+reactDom.render(<login />, document.getElementById('login'));
